8efed668df0fd88b16142d877bec5ed6c2e9ad29
Mark-Malysa <marmal22@bergen.org>
2021-06-12 15:18:36 -0400

    Changed Project Idea
    We decided to create an extention with a bunch of useful links and spend more time on designing it.

--- content.js
+++
@@ -1,4 +0,0 @@
-var divs = documents.getElementsByTagName('img');
-for (var i = 0, l = divs.lenght; i < l; i++){
-    divs[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;  
-}
